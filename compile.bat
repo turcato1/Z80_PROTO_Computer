ebda8e24200d7f5cb005abdd1e5eb9ea72fdd9ef
Thiago Turcato do Rego <turcato1@uol.com.br>
2023-12-31 15:13:56 -0300

    Files completed and rearranged

--- compile.bat
+++
@@ -1,5 +0,0 @@
-cls
-sjasmplus --sld=Z80_Cool_BIOS.sld --lst=Z80_Cool_BIOS.lst --raw=Z80_Cool_BIOS.obj --fullpath Z80_Cool_BIOS.asm
-@echo off
-rem sjasmplus --sld=Z80_Cool_BIOS.sld --fullpath Z80_Cool_BIOS.asm
-rem z80asm -b -s -m -l -g Z80_Cool_BIOS.asm
